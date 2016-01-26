9d83efae430add878c3d9149a9b0904d99bede7e
Taylor Stadeli <stadeli16@up.edu>
2016-01-25 18:35:58 -0800

    updated player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,7 @@ public class PlayerController : MonoBehaviour
 {
 
 
-	private float moveSpeed = 5f;
-
-
+	private float moveSpeed = 3f;
 
 	void Start()
 	{
@@ -17,29 +15,40 @@ public class PlayerController : MonoBehaviour
 	void Update()
 	{
 
+ 
 
-
-		if(Input.GetKeyDown (KeyCode.D))
+		if((Input.GetKey(KeyCode.RightArrow))||(Input.GetKey(KeyCode.D)))
 		{
-
 			transform.position += new Vector3(moveSpeed * Time.deltaTime, 0.0f,0.0f);
 		}
-
-
-		if(Input.GetKeyDown (KeyCode.A))
+		if((Input.GetKey(KeyCode.LeftArrow))||(Input.GetKey(KeyCode.A)))
 		{
-
 			transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0.0f, 0.0f);
 		}
-		if(Input.GetKeyDown (KeyCode.W))
+		if((Input.GetKey(KeyCode.UpArrow))||(Input.GetKey(KeyCode.W)))
 		{
-
 			transform.position += new Vector3(0.0f, moveSpeed * Time.deltaTime, 0.0f);
 		}
-		if(Input.GetKeyDown (KeyCode.S))
+		if((Input.GetKey(KeyCode.DownArrow))||(Input.GetKey(KeyCode.S)))
 		{
-
 			transform.position -= new Vector3(0.0f, moveSpeed * Time.deltaTime, 0.0f);
 		}
 	}
+
+//	private void OnTriggerEnter2D (Collider2D other)
+//	{
+//		//Check if the tag of the trigger collided with is Exit.
+//		if(other.tag == "Circle 1")
+//		{
+//			transform.Rotate(Vector3.forward * 7);
+//		}
+//
+//		if(other.tag == "Circle 2")
+//		{
+//			transform.Rotate (Vector3.forward * -2);
+//		}
+//	}
+
+
+
 }
