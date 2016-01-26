db49a4e586e1ede2fa1bd9e9a1b7a909a33adf15
Taylor Stadeli <stadeli16@up.edu>
2016-01-25 17:44:54 -0800

    Player added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+	private float moveSpeed = 5f;
+
+
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+
+
+
+		if(Input.GetKeyDown (KeyCode.D))
+		{
+
+			transform.position += new Vector3(moveSpeed * Time.deltaTime, 0.0f,0.0f);
+		}
+
+
+		if(Input.GetKeyDown (KeyCode.A))
+		{
+
+			transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+		}
+		if(Input.GetKeyDown (KeyCode.W))
+		{
+
+			transform.position += new Vector3(0.0f, moveSpeed * Time.deltaTime, 0.0f);
+		}
+		if(Input.GetKeyDown (KeyCode.S))
+		{
+
+			transform.position -= new Vector3(0.0f, moveSpeed * Time.deltaTime, 0.0f);
+		}
+	}
+}
